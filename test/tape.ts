ef946975ee2f975b078e963636981cfdf58fab3a
cmd <cmd@yggdrasil.onion>
2024-03-27 19:43:23 -0500

    update

--- test/tape.ts
+++ test/tape.ts
@@ -7,15 +7,15 @@ import e2e_settle_test  from './src/e2e/settle.test.js'
 
 // import vm_test  from './src/vm/vm.test.js'
 
-const local_config = {
-  network  : 'regtest',
-  verbose  : false,
-  core_params : [ '-reindex' ]
-}
+// const local_config = {
+//   network  : 'regtest',
+//   verbose  : false,
+//   core_params : [ '-reindex' ]
+// }
 
 tape('Escrow Core Test Suite', async t => {
   // vm_test(t)
-  const core   = get_daemon(local_config)
+  const core   = get_daemon()
   const client = await core.startup()
 
   await e2e_settle_test(client, t)
